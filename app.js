bcdf52aed558a18da9db1c18d5221b9fa8c7bd2d
Tom <tindy360@gmail.com>
2016-11-03 18:28:18 -0400

    Working version

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 //Event listner and prevent page form reloading.
-('.shopping-list-entry').onSubmit(function(event) {
-    event.preventDefault();
-
-    // Add/append item submitted below form
-    ('.shopping-list').append(
-        '<li>' +
-        '<span class="shopping-item"> +$(".shopping-list-entry") </span>' +
-        '<div class="shopping-item-controls">' +
-        '<button class="shopping-item-toggle">' +
-        '<span class="button-label">check</span>' +
-        '</button>' +
-        '<button class="shopping-item-delete">' +
-        '<span class="button-label">delete</span>' +
-        '</button>' +
-        '</div>' +
-        '</li>'
-    );
-
-    //reset webform to clear out for another input.
-    $('.shopping-list-entry').reset()
-
-    //logic for button click line through "shopping list checked"
-    $('.shopping-list').on("click", '.shopping-item-toggle', function(event) {
-        $(this).closest("<li>").toggleClass('.shopping-item__checked');
-    });
+$('#js-shopping-list-form').on('submit', function(event) {
+   event.preventDefault();
+   console.log('#js-shopping-list-form')
 
 
-    //Logic for deleting an item "shopping-item-delete"
-    $('.shopping-list').on("click", '.shopping-item-delete', function(event) {
-        $(this).closest("<li>").toggleClass('.shopping-item', .removeClass);
-    });
+   // Add/append item submitted below form
+   $('.shopping-list').append(
+     "<li>" +
+       "<span class='shopping-item'>" + $('#shopping-list-entry').val() +
+       //" < /span>" +
+       "<div class='.shopping-item-controls'>" +
+       "<button class='.shopping-item-toggle'>" +
+       "<span class='.button-label'>check</span>" +
+       "</button>" +
+       "<button class='.shopping-item-delete'>" +
+       "<span class='.button-label'>delete</span>" +
+       "</button>" +
+       "</div>" +
+       "</li>"
+   );
 
+   //reset webform to clear out for another input.
+   $(this)[0].reset()
+});
+
+  // toggle the .shopping-item__checked class
+  $('ul').on('click', '.shopping-item-toggle', function() {
+  		$(this.closest('.shopping-item-controls')).prev().toggleClass('shopping-item__checked');
+
+
+     console.log($(this));
+});
+
+
+// logic for deleting items from list
+$('.shopping-list').on('click', '.shopping-item-delete', function(event){
+ $(this).closest('li').remove();
+ console.log($(this).closest('li'))
 });
